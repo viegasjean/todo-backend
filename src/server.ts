415dd044547a86e23962efd3ebf2f80b446f4b1a
Jean Viegas <jeanmelg@gmail.com.br>
2022-07-23 13:40:30 -0300

    chore: install express and types

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,16 @@
-console.log('test ts-node-dev.');
+import express, { Request, Response } from 'express';
+
+const app = express();
+
+const PORT = 3000;
+
+app.use(express.json());
+
+app.get('/', (req: Request, res: Response) => {
+  return res.send('test express')
+})
+
+app.listen(PORT, () => {
+  console.log('server running on port ' + PORT);
+
+})
